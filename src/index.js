2394d7f79a73b0e500d604601783246a05deb663
ToffyChanachai <toffy.chanachai123@gmail.com>
2023-02-22 10:04:01 +0700

    update

--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-<<<<<<< HEAD
   res.send("Hello");
-=======
-  res.send("Bye KrubPom");
->>>>>>> df93588732cad0be6b0d065787acea61c5de9fae
 });
 
 app.listen(port, () => {
